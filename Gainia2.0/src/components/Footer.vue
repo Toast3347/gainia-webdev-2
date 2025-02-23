31058eff1ce8da4c81d4d2e8a35ddfce52f1058c
Toast3347 <706971@student.inholland.nl>
2025-02-23 22:14:31 +0100

    Header and footer

---
+++ Gainia2.0/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer class="footer mt-auto py-3">
+        <div class="d-flex align-items-center ps-3">
+            <img src="@/assets/images/Gainia_logo_transparentv2.png" alt="Gainia Logo" class="me-3 img-fluid" />
+            <span>&copyCopyright 2025 Gainia Inc&reg</span>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Gainia2.0/src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+    <header data-bs-theme="light" class="sticky-top">
+        <nav class="navbar navbar-expand-md">
+
+            <div class="container-fluid d-flex align-items-center">
+                <a href="/dashboard"><img src="@/assets/images/Gainia_logo_transparentv2.png" alt="Gainia Logo" class=" mx-4 rounded img-fluid" style="width: 100px;"></a>
+                <a href="home"><img src="@/assets/images/Gainia_logo_transparentv2.png" alt="Gainia Logo" class=" mx-4 rounded img-fluid" style="width: 100px;"></a>
+            </div>
+            <div class = "container-fluid d-flex align-items-center">
+                <a class="nav-link me-3" href="/dashboard">Dashboard</a> <a class="nav-link me-3" href="/goals">Goals</a> <a class="nav-link me-3" href="/exercise">Exercises</a>
+                <a class="nav-link me-3" href="/routine">Routines</a>
+                <a class="nav-link me-3" href="/notloggedin">Dashboard</a> <a class="nav-link me-3" href="/notloggedin">Statistics</a>
+            </div>
+
+                <a href="/home" class="btn btn-warning d-flex me-3">Logout</a>
+                <a href="/login" class="btn btn-primary d-flex me-3">Login</a> 
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
